1c487e3f1741669eb5cdce451c115e3675a5b221
chenqi2 <chenqi2@jtexpress.com>
2026-05-09 00:25:10 +0800

    fix: correct branch name from main to master in deploy scripts

--- deploy/install.sh
+++ deploy/install.sh
@@ -2,7 +2,7 @@
 #
 # Sub2API Installation Script
 # Sub2API 安装脚本
-# Usage: curl -sSL https://raw.githubusercontent.com/qi758771475-gif/sub2api-main/main/deploy/install.sh | bash
+# Usage: curl -sSL https://raw.githubusercontent.com/qi758771475-gif/sub2api-main/master/deploy/install.sh | bash
 #
 
 set -e
